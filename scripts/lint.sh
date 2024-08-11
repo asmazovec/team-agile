d1c11132abd74486e35bb8e70d8da4d96728af68
asmazovec <asmazovec@yandex.ru>
2024-08-12 00:58:02 +0700

    (#4) Add GitHook running lint and test before push

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,9 +5,13 @@ cd "$PROJ_DIR" || exit
 
 source ./scripts/prompt.sh
 
-prompt "Running lint"
+STATUS=0
 
-golangci-lint run ./...
-govulncheck ./...
+prompt "Running golangci-lint"
+if ! golangci-lint run ./...; then STATUS=1; fi
+
+prompt "Running govulncheck"
+if ! govulncheck ./...; then STATUS=1; fi
 
 end
+exit "$STATUS"
